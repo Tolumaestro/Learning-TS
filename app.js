a030ff88553aef9f26bcad29b8859dcc69920e41
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-08 15:32:28 +0100

    array type

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: "Tolulope",
-    age: 21
+    age: 21,
+    hobbies: ["running", "cooking"]
 };
+var favActivities;
+favActivities = ["sports", "tv"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "Tolulope",
   age: 21,
+  hobbies: ["running", "cooking"],
 };
 
+let favActivities: string[];
+favActivities = ["sports", "tv"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
