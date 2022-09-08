a3080936082757675f066436f31888bc6693bf40
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-08 16:44:59 +0100

    literal types

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     if (typeof input1 === "number" && typeof input2 === "number") {
         result = input1 + input2;
@@ -6,9 +6,17 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
+    if (resultConversion === "as-number") {
+        result = +result;
+    }
+    else if (resultConversion === "as-string") {
+        result = result.toString();
+    }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Tolu", "Soetan");
+var combinedStringAges = combine(30, 26, "as-string");
+console.log(combinedStringAges);
+var combinedNames = combine("Tolu", "Soetan", "as-string");
 console.log(combinedNames);
