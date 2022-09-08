6c6c6a85dc90236ad3fe9066aa976822f1b0a2b5
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-08 15:10:13 +0100

    working with strings and booleans

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        return phrase + result;
+    }
+    else {
+        return null;
+    }
 }
 var num1 = 5;
 var num2 = 2.8;
-var result = add(num1, num2);
+var printResult = false;
+var resultPhrase = "Result is: ";
+var result = add(num1, num2, printResult, resultPhrase);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    return phrase + result;
+  } else {
+    return null;
+  }
 }
 
 const num1 = 5;
 const num2 = 2.8;
+const printResult = false;
+const resultPhrase = "Result is: ";
 
-const result = add(num1, num2);
+const result = add(num1, num2, printResult, resultPhrase);
 
 console.log(result);
