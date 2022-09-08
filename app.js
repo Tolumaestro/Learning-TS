022a18fc78716e2cae057a6845c61ce4e5579981
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-08 15:26:34 +0100

    object type

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        return phrase + result;
-    }
-    else {
-        return null;
-    }
-}
-var num1 = 5;
-var num2 = 2.8;
-var printResult = false;
-var resultPhrase = "Result is: ";
-var result = add(num1, num2, printResult, resultPhrase);
-console.log(result);
+var person = {
+    name: "Tolulope",
+    age: 21
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,17 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    return phrase + result;
-  } else {
-    return null;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Tolulope",
+  age: 21,
+};
 
-const num1 = 5;
-const num2 = 2.8;
-const printResult = false;
-const resultPhrase = "Result is: ";
-
-const result = add(num1, num2, printResult, resultPhrase);
-
-console.log(result);
+console.log(person.name);
