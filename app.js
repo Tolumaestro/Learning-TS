1ee9f02979502933306ac21f7860009497219c18
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-09 11:15:50 +0100

    type any

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-function add(n1, n2) {
-    var result = n1 + n2;
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Tolu";
+// userName = userInput; won't work
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
--- app.ts
+++ app.ts
@@ -1,25 +1,10 @@
-function add(n1: number, n2: number) {
-  const result = n1 + n2;
-  return result;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Tolu";
+// userName = userInput; won't work
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
