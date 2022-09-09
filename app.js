4ec48991000560c6e990d5276b98b7ce1a902d76
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-09 11:09:22 +0100

    function type and callback

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
