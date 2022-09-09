c43fb85268a035e7ebde63cfaefa36e72e3f015e
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-09 11:03:36 +0100

    function types

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -6,3 +6,11 @@ function add(n1: number, n2: number) {
 function printResult(num: number): void {
   console.log("Result: " + num);
 }
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
